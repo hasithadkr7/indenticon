9041d658239322e06611593446ecdc1223135825
Hasitha Adikari <adhikari@globalwavenet.com>
2022-06-11 22:25:28 +0530

    identicon image generation

--- lib/indenticon.ex
+++ lib/indenticon.ex
@@ -1,18 +1,80 @@
 defmodule Indenticon do
   @moduledoc """
-  Documentation for `Indenticon`.
+  Moduel for generating `Identicon` images.
   """
 
-  @doc """
-  Hello world.
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
 
-  ## Examples
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
+  end
 
-      iex> Indenticon.hello()
-      :world
+  def draw_image(%Indenticon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
 
-  """
-  def hello do
-    :world
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Indenticon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+        {top_left, bottom_right}
+      end)
+
+    %Indenticon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Indenticon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {code, _} ->
+        rem(code, 2) == 0
+      end)
+
+    %Indenticon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Indenticon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Indenticon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
+  def pick_color(%Indenticon.Image{hex: [r, g, b | _tail]} = image) do
+    %Indenticon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Indenticon.Image{hex: hex}
   end
 end
